0a81dd4bbb1ee36aaf488afddaac4f13375158af
Suriyan <suriyant@gmail.com>
2014-04-05 06:44:14 +0700

    Add-on layout generate by cookiecutter template

---
+++ aldryn_icon/cms_plugins.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# from django.utils.translation import ugettext_lazy as _
+# from django.conf.urls import patterns, url
+
+# from cms.plugin_base import CMSPluginBase
+# from cms.plugin_pool import plugin_pool
+
+# from .models import Model
+# from .forms import Form
+# from .views import View
+
+
+# plugin_pool.register_plugin(CMSPlugin)
