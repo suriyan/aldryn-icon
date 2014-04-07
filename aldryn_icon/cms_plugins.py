ab1e83a6052a7aabe193b39ce2db863393787e5e
Suriyan <suriyant@gmail.com>
2014-04-07 11:20:09 +0700

    First working commit (using icon files from 'django-icons-tango')

--- aldryn_icon/cms_plugins.py
+++ aldryn_icon/cms_plugins.py
@@ -1,13 +1,46 @@
 # -*- coding: utf-8 -*-
-# from django.utils.translation import ugettext_lazy as _
-# from django.conf.urls import patterns, url
 
-# from cms.plugin_base import CMSPluginBase
-# from cms.plugin_pool import plugin_pool
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
-# from .models import Model
-# from .forms import Form
-# from .views import View
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from .models import Icon
 
 
-# plugin_pool.register_plugin(CMSPlugin)
+class IconPlugin(CMSPluginBase):
+    model = Icon
+    name = _("Icon")
+    render_template = "cms/plugins/icon.html"
+    text_enabled = True
+
+    fieldsets = (
+        (None, {
+            'fields': ('name',)
+        }),
+        (_('Optional Settings'), {
+            'classes': ('collapse',),
+            'fields': ('url', 'page_link', 'alt', 'float',),
+        }),
+    )
+
+    def render(self, context, instance, placeholder):
+        if instance.url:
+            link = instance.url
+        elif instance.page_link:
+            link = instance.page_link.get_absolute_url()
+        else:
+            link = ""
+        context.update({
+            'object': instance,
+            'link': link,
+            'placeholder': placeholder,
+            'STATIC_URL': settings.STATIC_URL,
+        })
+        return context
+
+    def icon_src(self, instance):
+        return settings.STATIC_URL + instance.name
+
+plugin_pool.register_plugin(IconPlugin)
