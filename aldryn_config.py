4d7771bbe8a4caf526dd82a3ef1445b7e88ccef4
Suriyan <suriyant@gmail.com>
2014-04-10 16:43:13 +0700

    Update 'aldryn_config.py' for plugin settings

--- aldryn_config.py
+++ aldryn_config.py
@@ -1,9 +1,13 @@
-# from aldryn_client import forms
-#
-#
-# class Form(forms.BaseForm):
-#     example = forms.CharField('Your example setting')
-#
-#     def to_settings(self, data, settings):
-#         settings['EXAMPLE_EXAMPLE'] = data['example']
-#         return settings
+from aldryn_client import forms
+
+
+class Form(forms.BaseForm):
+    icon_styles = forms.CharField('List of additional icon styles (comma separated)',
+                                  required=False)
+    icon_names = forms.CharField('List of additional icon names (comma separated)',
+                                 required=False)
+
+    def to_settings(self, data, settings):
+        settings['ICON_STYLES'] = data['icon_styles']
+        settings['ICON_NAMES'] = data['icon_names']
+        return settings
